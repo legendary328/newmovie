7ec2a83d0f7b43344465645c6eda7655a9573023
legendary328 <networkspheron@gmail.com>
2025-07-03 15:50:25 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '0fa1d78cc3d023d0d7e606d0e4783e31';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
